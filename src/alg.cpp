7aeaef0cd6dca7e0dc877f6d201e46d39423714a
AndreySavattev <78997292+AndreySavattev@users.noreply.github.com>
2021-04-08 10:08:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,7 +8,7 @@ int countPairs1(int *arr, int len, int value) {
      count++;
    }
   }
-  return 0;
+  return count;
 }
 
 int countPairs2(int *arr, int len, int value) {
@@ -31,41 +31,38 @@ int countPairs2(int *arr, int len, int value) {
          break;
       }
      }
-  return 0;
+  return count;
 }
 
 int countPairs3(int *arr, int len, int value) {
-  int count = 0, left = 0, right = len - 1;
+   int count = 0, left = 0, right = len - 1;
     while (left < right - 1) {
         int middle = (left + right) / 2;
-         if (arr[middle] <= value){
-          if (arr[middle] <= value)
-           left = middle;
-          else{
-           else
+        if (arr[middle] <= value)
+            left = middle;
+        else
             right = middle;
-          }
-         }
-        len = right - 1;
-       for (int i = 0; i <len; i++) {
+    }
+    len = right - 1;
+    
+
+    for (int i = 0; i <len; i++) {
         left = i+1, right = len-1;
-         int num = 0;
-          while (left < right) {
-           int m = (left + right) / 2;
-            if (arr[m] < (value-arr[i])){
-             if (arr[m] < (value-arr[i]))
-              left = m + 1;
-            } 
-            else{
-             else
-              right = m;
-            }
+        int countNum = 0;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < (value-arr[i]))
+                left = mid + 1;
+            else
+                right = mid;
         }
-    while (arr[left] == (value - arr[i])) {
-        num++;
-        left++;
+        while (arr[left] == (value - arr[i])) {
+            countNum++;
+            left++;
+        }
+        count += countNum;
     }
-        count += num;
-    }
-  return 0;
+    return count;
 }
+ 
+
